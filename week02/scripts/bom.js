54c8236f6feb748f07fdac37380ac2c9bcbaa0d0
ghrwn <gaelaherwin@gmail.com>
2025-07-24 08:10:54 -0600

    W02 Event Handling: BOM Chapter App complete

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -3,25 +3,26 @@ const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
 button.addEventListener('click', () => {
-  if (input.value.trim() === '') {
-    input.focus();
-    return;
+  const chapter = input.value.trim();
+
+  if (chapter !== '') {
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+
+    li.textContent = chapter;
+    deleteButton.textContent = '❌';
+    deleteButton.setAttribute('aria-label', `Remove ${chapter}`);
+
+    li.append(deleteButton);
+    list.append(li);
+
+    deleteButton.addEventListener('click', () => {
+      list.removeChild(li);
+      input.focus();
+    });
+
+    input.value = '';
   }
 
-  const li = document.createElement('li');
-  const deleteButton = document.createElement('button');
-
-  li.textContent = input.value;
-  deleteButton.textContent = '❌';
-  deleteButton.setAttribute('aria-label', `Remove ${input.value}`);
-
-  li.append(deleteButton);
-  list.append(li);
-
-  deleteButton.addEventListener('click', () => {
-    list.removeChild(li);
-  });
-
-  input.value = '';
   input.focus();
 });
